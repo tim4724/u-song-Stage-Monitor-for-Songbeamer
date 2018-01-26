76e3a890f4f6e70b9205f5b279c07f1e2b13f4b2
Tim <tim.vogel227@gmail.com>
2018-01-26 16:40:36 +0100

    Highlight Pre-Chorus, Pre-Bridge and Ending with new colors

--- src/main/java/com/tim/usong/core/entity/Section.java
+++ src/main/java/com/tim/usong/core/entity/Section.java
@@ -7,20 +7,28 @@ import java.util.Objects;
 
 public class Section {
 
-    public enum Type {VERSE, CHORUS, BRIDGE, NONE}//TODO: add types
+    public enum Type {VERSE, PRE_CHORUS, CHORUS, PRE_BRIDGE, BRIDGE, ENDING, UNKNOWN}
 
     private String name;
-    private Type type = Type.NONE;
+    private Type type;
     private List<Page> pages = new ArrayList<>();
 
     public Section(String name) {
         this.name = name;
         if (name.matches("(?i)^(Vers|Verse|Strophe)($| .*)")) {
             type = Section.Type.VERSE;
-        } else if (name.matches("(?i)^(Pre-Refrain|Refrain|Pre-Chorus|Chorus)($| .*)")) {
+        } else if (name.matches("(?i)^(Pre-Refrain|Pre-Chorus)($| .*)")) {
+            type = Type.PRE_CHORUS;
+        } else if (name.matches("(?i)^(Refrain|Chorus)($| .*)")) {
             type = Section.Type.CHORUS;
-        } else if (name.matches("(?i)^(Pre-Bridge|Bridge)($| .*)")) {
+        } else if (name.matches("(?i)^(Pre-Bridge)($| .*)")) {
+            type = Type.PRE_BRIDGE;
+        } else if (name.matches("(?i)^(Bridge)($| .*)")) {
             type = Section.Type.BRIDGE;
+        } else if (name.matches("(?i)^(Ending|Outro)($| .*)")) {
+            type = Type.ENDING;
+        } else {
+            type = Type.UNKNOWN;
         }
     }
 
