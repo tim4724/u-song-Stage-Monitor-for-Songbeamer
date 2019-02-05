e727b2cd4a4431850fc9cb4c8f0e4edeae7d9df6
vogelti64073 <vogelti64073@th-nuernberg.de>
2019-02-05 23:55:16 +0100

    Add "Done" button to tutorial page

--- src/main/java/com/tim/usong/ui/TutorialFrame.java
+++ src/main/java/com/tim/usong/ui/TutorialFrame.java
@@ -1,15 +1,24 @@
 package com.tim.usong.ui;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
 import java.util.prefs.Preferences;
 
 public class TutorialFrame extends WebFrame {
-
     public TutorialFrame() {
         super("", "http://localhost/tutorial",
                 Preferences.userNodeForPackage(TutorialFrame.class).node("tutorial"),
                 1024, 800, 1);
         ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle");
         setTitle(messages.getString("tutorial"));
+
+        Button closeButton = new Button(messages.getString("done"));
+        closeButton.addActionListener(e -> dispose());
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        bottomPanel.add(closeButton);
+        add(bottomPanel, BorderLayout.SOUTH);
     }
 }
